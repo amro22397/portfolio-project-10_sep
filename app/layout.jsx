34c62e42475c5cdb089da4d341977d2bdcd9c6d9
amro22397 <amroalmutasim22@gmail.com>
2024-09-21 08:50:15 +0400

    some change 2

--- app/layout.jsx
+++ app/layout.jsx
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"></link>
 
       <body
-        className={`${jetBrainsMono.variable} flex flex-col justify-center xl:max-w-[85%] mx-auto mb-12`}
+        className={`${jetBrainsMono.variable} flex flex-col justify-center xl:max-w-[85%] mx-auto mb-12 max-xl:mt-4`}
       >
         <AppProvider>
 
