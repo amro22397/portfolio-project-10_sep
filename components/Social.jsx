ef6a5f2de299fe51cf09fddac456c31f664e98b5
amro22397 <amroalmutasim22@gmail.com>
2024-10-23 06:36:04 +0400

    add whatsapp social

--- components/Social.jsx
+++ components/Social.jsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import React from 'react'
 
-import { FaGithub, FaLinkedin, FaYoutube, FaFacebook  } from 'react-icons/fa'
+import { FaGithub, FaLinkedin, FaYoutube, FaFacebook, FaWhatsapp  } from 'react-icons/fa'
 import { FaSquareUpwork } from "react-icons/fa6";
 
 
@@ -10,6 +10,7 @@ const socials = [
     { icon: 'FaGithub', path: "https://github.com/amro22397" },
     { icon: 'FaSquareUpwork', path: "https://www.upwork.com/freelancers/~01a0eb164053da3046" },
     { icon: 'FaFacebook', path: "https://www.facebook.com/amro97/" },
+    { icon: 'FaWhatsapp', path: "http://wa.me/+249995291772" },
     { icon: 'FaYoutube', path: "https://www.youtube.com/@AmroAlmutasim/videos" },
 ]
 
@@ -28,10 +29,16 @@ const Social = ({ containerStyles, iconStyles}) => {
       <Link href={socials[2].path} target="_blank">
       <FaSquareUpwork className={`${iconStyles} text-green-600`} />
       </Link>
+
       <Link href={socials[3].path} target="_blank">
       <FaFacebook className={`${iconStyles} text-blue-600`} />
       </Link>
+
       <Link href={socials[4].path} target="_blank">
+      <FaWhatsapp className={`${iconStyles} text-green-600`} />
+      </Link>
+
+      <Link href={socials[5].path} target="_blank">
       <FaYoutube className={`${iconStyles} text-red-600`} />
       </Link>
     </div>
