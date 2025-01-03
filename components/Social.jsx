41e60b94da71363630ddbf8b68f1b6a702ef0b4f
amro22397 <amroalmutasim22@gmail.com>
2025-01-03 15:52:39 +0400

    remove github link

--- components/Social.jsx
+++ components/Social.jsx
@@ -7,11 +7,10 @@ import { FaSquareUpwork } from "react-icons/fa6";
 
 const socials = [
   { icon: 'FaLinkedin', path: "https://www.linkedin.com/in/amro97/" },
-    { icon: 'FaGithub', path: "https://github.com/amro22397" },
-    { icon: 'FaSquareUpwork', path: "https://www.upwork.com/freelancers/~01a0eb164053da3046" },
     { icon: 'FaFacebook', path: "https://www.facebook.com/amro97/" },
     { icon: 'FaWhatsapp', path: "http://wa.me/+249995291772" },
     { icon: 'FaYoutube', path: "https://www.youtube.com/@AmroAlmutasim/videos" },
+    { icon: 'FaSquareUpwork', path: "https://www.upwork.com/freelancers/~01a0eb164053da3046" },
 ]
 
 
@@ -23,23 +22,19 @@ const Social = ({ containerStyles, iconStyles}) => {
       </Link>
 
       <Link href={socials[1].path} target="_blank">
-      <FaGithub className={`${iconStyles} text-black`} />
-      </Link>
-
-      <Link href={socials[2].path} target="_blank">
-      <FaSquareUpwork className={`${iconStyles} text-green-600`} />
-      </Link>
-
-      <Link href={socials[3].path} target="_blank">
       <FaFacebook className={`${iconStyles} text-blue-600`} />
       </Link>
 
-      <Link href={socials[4].path} target="_blank">
+      <Link href={socials[2].path} target="_blank">
       <FaWhatsapp className={`${iconStyles} text-green-600`} />
       </Link>
 
-      <Link href={socials[5].path} target="_blank">
+      <Link href={socials[3].path} target="_blank">
       <FaYoutube className={`${iconStyles} text-red-600`} />
+      </Link>
+
+      <Link href={socials[4].path} target="_blank">
+      <FaSquareUpwork className={`${iconStyles} text-green-600`} />
       </Link>
     </div>
   )
