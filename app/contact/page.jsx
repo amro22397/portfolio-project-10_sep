bb137d8407dcfed423afbc5b1c2309b129fa79b3
amro22397 <amroalmutasim22@gmail.com>
2025-01-30 16:02:19 +0400

    fix contact form

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -92,7 +92,8 @@ const handleChange = () => {
     <motion.section 
     initial={{opacity: 0}}
     animate={{opacity: 1, transition: {delay: 2.4, duration: 0.4, ease: "easeIn"}}}
-    className='py-6 max-xl:max-w-[90vw] mx-auto'
+    className='py-6 max-w-[100vw] sm:max-w-[575px]
+     xl:max-w-[1400px] mx-auto'
     >
       <div className="containter mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px]">
@@ -100,7 +101,7 @@ const handleChange = () => {
           <div className="xl:h-[54%] order-2 xl:order-none">
 
             <form onSubmit={handleSubmit}
-            className="flex flex-col gap-6 p-10 bg-[#27272c] dark:bg-[#414149] rounded-xl">
+            className="flex flex-col gap-6 p-10 bg-[#27272c] dark:bg-[#414149] xl:rounded-xl rounded-none">
               <h3 className="text-4xl text-orange-400">Let's work together</h3>
               <p className="text-white/60">
               I'm happy to get request for web designs and I will make it as soon as I can...
